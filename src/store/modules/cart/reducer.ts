fe2239b764821c537e3b7ec2bb98529dc07ced1e
André Victor <andrevictor50@gmail.com>
2020-06-27 14:58:54 -0300

    🚀 added no duplicated products

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -13,10 +13,18 @@ interface ICartAction extends Action{
 const Cart: Reducer<any, ICartAction> = (state = [], action: ICartAction) => {
   switch(action.type) {
     case 'ADDTOCART':
-      return [...state, {
-        ...action.product,
-        amount: 1,
-      }];
+      return produce(state, (draft: any[]) => {
+        const productIndex = draft.findIndex(product => product.id === action.product.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
