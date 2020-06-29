1ca4210351a99b8311412e7d43fbd047aaa312ef
André Victor <andrevictor50@gmail.com>
2020-06-29 12:05:48 -0300

    🚀 added stock

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -7,11 +7,20 @@ import { addToCartSuccess, updateAmount } from './actions';
 function* addToCart({ id }: any ) {
   const productExists = yield select(
     state => state.cart.find((product: any) => product.id === id),
-  )
+  );
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    return;
+  }
 
   if (productExists) {
-    const amount = productExists.amount + 1;
-
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
